988b2afb4d7f5a811f7f4d8c8540fc20822dd97f
Wendell Rocha <wendellrochaa@gmail.com>
2025-10-15 23:02:10 +0000

    makes npm test more robust with async-retry and orchestrator.js

---
+++ tests/orchestrator.js
@@ -0,0 +1,21 @@
+import retry from "async-retry";
+
+async function waitForAllService() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 1000,
+    });
+
+    async function fetchStatusPage() {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      if (!response.ok) {
+        throw new Error();
+      }
+    }
+  }
+}
+
+export default { waitForAllService };
