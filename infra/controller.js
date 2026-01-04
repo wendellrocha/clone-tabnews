465dffd5c85c6334b276ce6ddebfb2c0e0d3bb95
Wendell Rocha <wendellrochaa@gmail.com>
2026-01-04 16:37:25 +0000

    feat: exoire session and clear token

--- infra/controller.js
+++ infra/controller.js
@@ -14,11 +14,12 @@ function onNoMatchHandler(request, response) {
 }
 
 function onErrorHandler(error, request, response) {
-  if (
-    error instanceof ValidationError ||
-    error instanceof NotFoundError ||
-    error instanceof UnauthorizedError
-  ) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return response.status(error.statusCode).json(error);
+  }
+
+  if (error instanceof UnauthorizedError) {
+    clearSessionCookie(response);
     return response.status(error.statusCode).json(error);
   }
 
@@ -42,8 +43,20 @@ function setSessionCookie(response, sessionObject) {
   response.setHeader("Set-Cookie", setCookieHeader);
 }
 
+function clearSessionCookie(response) {
+  const setCookieHeader = cookie.serialize("session_id", "invalid", {
+    path: "/",
+    maxAge: -1,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
+
+  response.setHeader("Set-Cookie", setCookieHeader);
+}
+
 const controller = {
   setSessionCookie,
+  clearSessionCookie,
   errorHandlers: {
     onNoMatch: onNoMatchHandler,
     onError: onErrorHandler,
