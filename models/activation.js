2394015b59606e06c4d10279137d9fb71764e212
Wendell Rocha <wendellrochaa@gmail.com>
2026-01-10 21:10:06 +0000

    feat: send activation link in registration email

---
+++ models/activation.js
@@ -0,0 +1,72 @@
+import email from "infra/email";
+import database from "infra/database";
+import webserver from "infra/webserver";
+
+const EXPIRATION_IN_MILLISECONDS = 60 * 15 * 1000; // 15 minutes
+
+async function create(userId) {
+  const expiresAt = new Date(Date.now() + EXPIRATION_IN_MILLISECONDS);
+
+  const newToken = await runInsertQuery(userId, expiresAt);
+  return newToken;
+
+  async function runInsertQuery(userId, expiresAt) {
+    const results = await database.query({
+      text: `
+        INSERT INTO
+          user_activation_tokens (user_id, expires_at)
+        VALUES
+          ($1, $2)
+        RETURNING *
+      ;`,
+      values: [userId, expiresAt],
+    });
+
+    return results.rows.at(0);
+  }
+}
+
+async function sendEmailToUser(user, activation) {
+  await email.send({
+    from: "TabNews <contato@gmail.com>",
+    to: user.email,
+    subject: "Ative seu cadastro!",
+    text: `${user.username}, clique no link abaixo para ativar seu cadastro:
+    
+${webserver.origin}/cadastro/ativar/${activation.id}
+
+Atenciosamente,
+Equipe TabNews
+    `,
+  });
+}
+
+async function findOneByUserId(userId) {
+  const newToken = await runSelectQuery(userId);
+  return newToken;
+
+  async function runSelectQuery(userId) {
+    const results = await database.query({
+      text: `
+        SELECT
+          *
+        FROM
+          user_activation_tokens
+        WHERE
+          user_id = $1
+        LIMIT 1
+      ;`,
+      values: [userId],
+    });
+
+    return results.rows.at(0);
+  }
+}
+
+const activation = {
+  create,
+  sendEmailToUser,
+  findOneByUserId,
+};
+
+export default activation;
--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -1,3 +1,4 @@
+import activation from "models/activation";
 import orchestrator from "tests/orchestrator";
 import { version as uuidVersion } from "uuid";
 
@@ -9,6 +10,7 @@ beforeAll(async () => {
 });
 
 describe("Use case: Registration Flow (all successful", () => {
+  let createUserResponseBody;
   test("Create user account", async () => {
     const createUserResponse = await fetch(
       "http://localhost:3000/api/v1/users",
@@ -18,32 +20,44 @@ describe("Use case: Registration Flow (all successful", () => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          username: "ResgistrationFlow",
+          username: "RegistrationFlow",
           email: "registration.flow@gmail.com",
           password: "Str0ngP@ssword!",
         }),
       },
     );
 
-    const responseBody = await createUserResponse.json();
+    createUserResponseBody = await createUserResponse.json();
 
     expect(createUserResponse.status).toBe(201);
-    expect(responseBody).toEqual({
-      id: responseBody.id,
-      username: responseBody.username,
-      email: responseBody.email,
-      password: responseBody.password,
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: createUserResponseBody.username,
+      email: createUserResponseBody.email,
+      password: createUserResponseBody.password,
       features: ["read:activation_token"],
-      created_at: responseBody.created_at,
-      updated_at: responseBody.updated_at,
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
     });
 
-    expect(uuidVersion(responseBody.id)).toBe(4);
-    expect(Date.parse(responseBody.created_at)).not.toBeNaN();
-    expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+    expect(uuidVersion(createUserResponseBody.id)).toBe(4);
+    expect(Date.parse(createUserResponseBody.created_at)).not.toBeNaN();
+    expect(Date.parse(createUserResponseBody.updated_at)).not.toBeNaN();
   });
 
-  test("Receive activation email", async () => {});
+  test("Receive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<contato@gmail.com>");
+    expect(lastEmail.recipients[0]).toBe("<registration.flow@gmail.com>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro!");
+    expect(lastEmail.text).toContain("RegistrationFlow");
+    expect(lastEmail.text).toContain(activationToken.id);
+  });
 
   test("Activate account", async () => {});
 
