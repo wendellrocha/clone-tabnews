49b2bb19da7939656b0bf1f8fcd263bab363c2d6
Wendell Rocha <wendellrochaa@gmail.com>
2026-01-18 20:50:19 +0000

    feat: consider `resource` in `authorization` model

--- models/authorization.js
+++ models/authorization.js
@@ -1,5 +1,17 @@
-function can(user, feature) {
-  return user.features.includes(feature);
+function can(user, feature, resource) {
+  if (feature === "update:user" && resource) {
+    if (user.id === resource.id) {
+      return true;
+    }
+
+    return false;
+  }
+
+  if (user.features.includes(feature)) {
+    return true;
+  }
+
+  return false;
 }
 
 const authorization = {
