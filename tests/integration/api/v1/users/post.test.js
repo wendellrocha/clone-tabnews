47f56588e59b705ab668b6ef19ef03601c720254
Wendell Rocha <wendellrochaa@gmail.com>
2026-01-18 03:44:14 +0000

    feat: require `create:user` to access `/api/v1/users`

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -136,5 +136,39 @@ describe("POST /api/v1/users", () => {
         status_code: 400,
       });
     });
+
+    describe("Default user", () => {
+      test("With unique and valid data", async () => {
+        const user1 = await orchestrator.createUser({});
+        await orchestrator.activateUser(user1.id);
+        const user1SessionObject = await orchestrator.createSession(user1.id);
+
+        const user2Response = await fetch(
+          "http://localhost:3000/api/v1/users",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Cookie: `session_id=${user1SessionObject.token}`,
+            },
+            body: JSON.stringify({
+              username: "wendellrocha2",
+              email: "email@example.com",
+              password: "123",
+            }),
+          },
+        );
+
+        expect(user2Response.status).toBe(403);
+
+        const responseBody = await user2Response.json();
+        expect(responseBody).toEqual({
+          name: "ForbiddenError",
+          message: "Você não possui permissão para executar esta ação.",
+          action: 'Verifique se o usuário tem a feature: "create:user"',
+          status_code: 403,
+        });
+      });
+    });
   });
 });
